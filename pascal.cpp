ca9ae4ec6978f67765f9c6e15037121d8241cb04
Eduardofig <duzinho039@gmail.com>
2021-05-18 04:13:01 +0000

    pascal_number

--- pascal.cpp
+++ pascal.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-int pascal(int row, int column)
+int pascal_number(int row, int column)
 {
     if(column == 1 || column == row || row == 1) {
         return 1;
     }
-    return pascal(row - 1, column) + pascal(row - 1, column - 1);
+    return pascal_number(row - 1, column) + pascal_number(row - 1, column - 1);
 }
 
 void pascal_triangle(int rows) 
 {
     for (int i = 1; i < rows; ++i) {
         for (int j = 1; j < i + 1; ++j) {
-            std::cout << pascal(i, j) << " ";
+            std::cout << pascal_number(i, j) << " ";
         }
         std::cout << std::endl;
     }
