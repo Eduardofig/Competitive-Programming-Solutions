021f084df7f1ecc7c967178625a4c913698cb587
Eduardofig <duzinho039@gmail.com>
2021-05-24 03:01:56 +0000

    icc

---
+++ icc/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+typedef struct 
+{
+    int x, y;
+}ponto;
+
+int main(int argc, char *argv[])
+{
+    //Declaracao das variaveis
+    ponto re1[4], re2_centro;
+    int altura1, altura2, largura1, largura2;
+    //Leitura das variaveis
+    scanf("%d %d %d %d %d %d %d %d", &re1[0].x, &re1[0].y, &largura1, &altura1, &re2_centro.x, &re2_centro.y, &largura2, &altura2); 
+    re1[1].x = re1[0].x + largura1; re1[1].y = re1[0].y;
+    re1[2].x = re1[0].x + largura1; re1[2].y = re1[0].y + altura1;
+    re1[3].x = re1[0].x; re1[3].y = re1[0].y + altura1;
+    if(re1[0].x == re2_centro.x && re1[0].y == re2_centro.y && re1[1].x == re2_centro.x + largura2 && re1[1].y == re2_centro.y + altura2) {
+        printf("\n");
+    }
+    for (int i = 0; i < 4; ++i) {
+        if(re1[i].x > re2_centro.x && re1[i].x < re2_centro.x + largura2 && re1[i].y > re2_centro.y && re1[i].y < re2_centro.y + altura2) {
+            switch(i)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+    printf("MISS\n");
+    return 0;
+    //Impressao do resultado
+}
