35d5bfc3d5bc9184362340572247ef7754473d8a
Eduardofig <duzinho039@gmail.com>
2021-07-11 20:08:49 +0000

    :poop: BAD CODE: Precisa resolver os bo

---
+++ icc/trabalho-2/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 11/07/2021 16:19:27 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [Trabalho 2] Admirável Mundo Caótico
--- icc/trabalho-2/main.c
+++ icc/trabalho-2/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define VON_NEUMANN 'N'
 #define MOORE 'M'
-#define VIVA '#'
+#define VIVA 'x'
 #define MORTA '.'
 
 int checar_se_esta_inbounds(int posicao_i, int posicao_j, int dimensao_m_matriz, int dimensao_n_matriz)
@@ -15,25 +16,29 @@ int checar_se_esta_inbounds(int posicao_i, int posicao_j, int dimensao_m_matriz,
 int contar_vizinhos_vivos(char modelo, char **matriz, int dimensao_m_matriz, int dimensao_n_matriz,
         int posicao_i, int posicao_j)
 {
-    int vetores_moore[6][2] = {
+    int vetores_moore[8][2] = {
         {0, 1},
         {1, 1},
         {-1, 1},
         {0, -1},
         {1, -1},
-        {-1, -1}
+        {-1, -1},
+        {1, 0},
+        {-1, 0}
     },
     posicao_i_vizinho, posicao_j_vizinho, n_vizinhos_vivos = 0;
 
     switch(modelo) {
         case MOORE:
             //Itera por todos os vizinhos que estao na soma dos vetores a posicao atual
-            for(int i = 0; i < 6; ++i) {
+            /*printf("[%d][%d]:\n", posicao_i, posicao_j);*/
+            for(int i = 0; i < 8; ++i) {
                 posicao_i_vizinho = posicao_i + vetores_moore[i][0];
                 posicao_j_vizinho = posicao_j + vetores_moore[i][1];
 
                 if(checar_se_esta_inbounds(posicao_i_vizinho, posicao_j_vizinho, dimensao_m_matriz, dimensao_n_matriz)) {
-                    if(matriz[posicao_i_vizinho][posicao_j_vizinho] == '#') 
+                    /*printf("[%d][%d] = %c\n", posicao_i_vizinho, posicao_j_vizinho, matriz[posicao_i_vizinho][posicao_j_vizinho]);*/
+                    if(matriz[posicao_i_vizinho][posicao_j_vizinho] == VIVA) 
                         ++n_vizinhos_vivos;
                 }
             }
@@ -66,10 +71,13 @@ void avaliar_geracao(char **matriz, char **copia, char modelo, int dimensao_m_ma
     for(int i = 0; i < dimensao_m_matriz; ++i) {
         for(int j = 0; j < dimensao_n_matriz; ++j) {
             n_vizinhos_vivos = contar_vizinhos_vivos(modelo, matriz, dimensao_m_matriz, dimensao_n_matriz, i, j);
+            /*printf("[%d][%d] = %c, n_vizinhos =  %d\n", i, j, matriz[i][j], n_vizinhos_vivos);*/
 
             if(matriz[i][j] == VIVA) {
-                if(n_vizinhos_vivos < 2 || n_vizinhos_vivos > 3)
+                if(n_vizinhos_vivos < 2 || n_vizinhos_vivos > 3) {
+                    printf("morrer: [%d][%d]\n", i, j);
                     copia[i][j] = MORTA;
+                }
             } else {
                 if(n_vizinhos_vivos == 3) copia[i][j] = VIVA;
             }
@@ -83,7 +91,7 @@ void imprimir_matriz(char **matriz, int dimensao_m_matriz, int dimensao_n_matriz
 {
     for(int i = 0; i < dimensao_m_matriz; ++i) {
         for(int j = 0; j < dimensao_n_matriz; ++j) {
-            printf("%d ", matriz[i][j]);
+            printf("%c", matriz[i][j]);
         }
         printf("\n");
     }
@@ -106,27 +114,27 @@ int main(int argc, char *argv[])
     for(int i = 0; i < m; ++i) {
         M[i] = (char*)malloc(sizeof(char)*n);
         copia[i] = (char*)malloc(sizeof(char)*n);
-    }
-
-    for(int i = 0; i < m; ++i) {
-        for(int j = 0; j < n; ++j) {
-            scanf("%c", M[i] + j);
-        }
+        scanf("%s", M[i]);
     }
 
     memcpy(copia, M, sizeof(char)*m*n);
 
     for(int i = 0; i < p; ++i) {
+        /*system("clear");*/
+        printf("\n");
+        printf("frame %d\n", i);
+        avaliar_geracao(M, copia, v, m, n, sizeof(char)*m*n);
         imprimir_matriz(M, m, n);
-        avaliar_geracao(M, copia, m, n, v, sizeof(char)*m*n);
+        /*usleep(800000);*/
     }
+    imprimir_matriz(M, m, n);
 
-    for(int i = 0; i < m; ++i) {
-        free(M[i]);
-        free(copia[i]);
-    }
-    free(M);
-    free(copia);
+    /*for(int i = 0; i < m; ++i) {*/
+        /*free(M[i]);*/
+        /*free(copia[i]);*/
+    /*}*/
+    /*free(M);*/
+    /*free(copia);*/
 
     return 0;
 }
