a1f4cc2515449c6242561cfce89eaf4408d310fd
Eduardofig <duzinho039@gmail.com>
2021-07-13 04:09:59 +0000

    :rocket: DEPLOY: Trabalho 2 icc finalizado

--- icc/trabalho-2/main.c
+++ icc/trabalho-2/main.c
@@ -8,66 +8,62 @@
 #define VIVA 'x'
 #define MORTA '.'
 
-void colocar_inbounds(int *posicao_i, int *posicao_j, int dimensao_m_matriz, int dimensao_n_matriz)
+//Funcao que mantem as posicoes de vizinhanca dentro da matriz, dando a volta para o outro lado se 
+//preciso
+void manter_inbounds(int *posicao_i, int *posicao_j, int dimensao_m_matriz, int dimensao_n_matriz)
 {
-    if(*posicao_i >= dimensao_m_matriz) *posicao_i = 0;
-    else if(*posicao_i < 0) *posicao_i = dimensao_m_matriz - 1;
+    if(*posicao_i >= dimensao_m_matriz) *posicao_i -= dimensao_m_matriz;
+    else if(*posicao_i < 0) *posicao_i += dimensao_m_matriz;
 
-    if(*posicao_j >= dimensao_n_matriz) *posicao_j = 0;
-    else if(*posicao_j < 0) *posicao_j = dimensao_n_matriz - 1;
+    if(*posicao_j >= dimensao_n_matriz) *posicao_j -= dimensao_n_matriz;
+    else if(*posicao_j < 0) *posicao_j += dimensao_n_matriz;
 }
 
+//Funcao que retorna o numero de vizinhos vivos de uma determinada celula
 int contar_vizinhos_vivos(char modelo, char **matriz, int dimensao_m_matriz, int dimensao_n_matriz,
         int posicao_i, int posicao_j)
 {
-    int vetores_moore[8][2] = {
-        {0, 1},
-        {1, 1},
-        {-1, 1},
-        {0, -1},
-        {1, -1},
-        {-1, -1},
-        {1, 0},
-        {-1, 0}
-    },
-        posicao_i_vizinho, posicao_j_vizinho, n_vizinhos_vivos = 0;
-
-    switch(modelo) {
-        case MOORE:
-            //Itera por todos os vizinhos que estao na soma dos vetores a posicao atual
-            /*printf("[%d][%d]:\n", posicao_i, posicao_j);*/
-            for(int i = 0; i < 8; ++i) {
-                posicao_i_vizinho = posicao_i + vetores_moore[i][0];
-                posicao_j_vizinho = posicao_j + vetores_moore[i][1];
-
-                /*printf("[%d][%d] = %c\n", posicao_i_vizinho, posicao_j_vizinho, matriz[posicao_i_vizinho][posicao_j_vizinho]);*/
-
-                colocar_inbounds(&posicao_i_vizinho, &posicao_j_vizinho, dimensao_m_matriz, dimensao_n_matriz);
-
-                if(matriz[posicao_i_vizinho][posicao_j_vizinho] == VIVA) 
-                    ++n_vizinhos_vivos;
+    int posicao_i_vizinho, posicao_j_vizinho, seletor_vetor, n_vizinhos_vivos = 0,
+        vetores_direcao[2][8][2] = {
+            {
+                {0, 1},
+                {0, 2},
+                {0, -1},
+                {0, -2},
+                {1, 0},
+                {2, 0},
+                {-1, 0},
+                {-2, 0}
+            },
+            {
+                {0, 1},
+                {1, 1},
+                {-1, 1},
+                {0, -1},
+                {1, -1},
+                {-1, -1},
+                {1, 0},
+                {-1, 0}
             }
+        };
 
-            break;
+    if(modelo == VON_NEUMANN) seletor_vetor = 0;
+    else if(modelo == MOORE) seletor_vetor = 1;
 
-        case VON_NEUMANN:
-            //Avalia todos os vizinhos na vertical
-            for(int i = 0; i < dimensao_m_matriz; ++i) {
-                if(i != posicao_i || matriz[i][posicao_j] == VIVA)
-                    ++n_vizinhos_vivos;
-            }
+    for(int i = 0; i < 8; ++i) {
+        posicao_i_vizinho = posicao_i + vetores_direcao[seletor_vetor][i][0];
+        posicao_j_vizinho = posicao_j + vetores_direcao[seletor_vetor][i][1];
 
-            //Avalia todos os vizinhos na horizontal
-            for(int j = 0; j < dimensao_n_matriz; ++j) {
-                if(j != posicao_j || matriz[posicao_i][j] == VIVA) 
-                    ++n_vizinhos_vivos;
-            }
+        manter_inbounds(&posicao_i_vizinho, &posicao_j_vizinho, dimensao_m_matriz, dimensao_n_matriz);
 
-            break;
+        if(matriz[posicao_i_vizinho][posicao_j_vizinho] == VIVA) 
+            ++n_vizinhos_vivos;
     }
+
     return n_vizinhos_vivos;
 }
 
+//Funcao que aplica todas as mortes e vidas de uma determinada geracao
 void avaliar_geracao(char **matriz, char **copia, char modelo, int dimensao_m_matriz, int dimensao_n_matriz)
 {
     int n_vizinhos_vivos;
@@ -75,16 +71,13 @@ void avaliar_geracao(char **matriz, char **copia, char modelo, int dimensao_m_ma
     for(int i = 0; i < dimensao_m_matriz; ++i) {
         for(int j = 0; j < dimensao_n_matriz; ++j) {
             n_vizinhos_vivos = contar_vizinhos_vivos(modelo, matriz, dimensao_m_matriz, dimensao_n_matriz, i, j);
-            /*printf("[%d][%d] = %c, n_vizinhos =  %d\n", i, j, matriz[i][j], n_vizinhos_vivos);*/
 
             if(matriz[i][j] == VIVA) {
                 if(n_vizinhos_vivos < 2 || n_vizinhos_vivos > 3) {
-                    /*printf("morrer: [%d][%d]\n", i, j);*/
                     copia[i][j] = MORTA;
                 }
             } else {
                 if(n_vizinhos_vivos == 3) {
-                    /*printf("viver: [%d][%d]\n", i, j);*/
                     copia[i][j] = VIVA;
                 }
             }
@@ -95,6 +88,7 @@ void avaliar_geracao(char **matriz, char **copia, char modelo, int dimensao_m_ma
         memcpy(matriz[i], copia[i], sizeof(char)*dimensao_n_matriz);
 }
 
+//Funcao que imprime a matriz
 void imprimir_matriz(char **matriz, int dimensao_m_matriz, int dimensao_n_matriz)
 {
     for(int i = 0; i < dimensao_m_matriz; ++i) {
@@ -113,7 +107,7 @@ int main(int argc, char *argv[])
     scanf("%d %d %d %c", &m, &n, &p, &v);
 
     if(m <= 0 || n <= 0 || p <= 0) {
-        printf("Dados da entrada apresentam erro.\n");
+        printf("Dados de entrada apresentam erro.\n");
         exit(0);
     }
 
@@ -121,34 +115,44 @@ int main(int argc, char *argv[])
 
     scanf(" ");
 
+    //scan das matrizes
     for(int i = 0; i < m; ++i) {
         M[i] = (char*)malloc(sizeof(char)*n);
         copia[i] = (char*)malloc(sizeof(char)*n);
-        for(int j = 0; j < n - 1; ++j) {
+
+        for(int j = 0; j < n; ++j) {
             scanf("%c", M[i] + j);
+            if(M[i][j] != VIVA && M[i][j] != MORTA) {
+                printf("Dados de entrada apresentam erro.\n");
+                for(int k = 0; k < i; ++k) {
+                    free(M[k]);
+                    free(copia[k]);
+                }
+                free(M);
+                free(copia);
+                exit(0);
+            }
+            scanf("\n");
         }
-        scanf("%c\n", M[i] + n - 1);
+
+        memcpy(copia[i], M[i], sizeof(char)*n);
     }
 
-    for(int i = 0; i < m; ++i)
-        memcpy(copia[i], M[i], sizeof(char)*n);
+    for(int i = 0; i < p; ++i) {
+        system("clear");
+        imprimir_matriz(M, m, n);
+        avaliar_geracao(M, copia, v, m, n);
+        usleep(3e5);
+    }
 
     imprimir_matriz(M, m, n);
-    for(int i = 0; i < p; ++i) {
-        /*system("clear");*/
-        avaliar_geracao(M, copia, v, m, n);
-        printf("\n");
-        imprimir_matriz(M, m, n);
-        /*usleep(800000);*/
-    }
-    printf("final.\n");
 
-    /*for(int i = 0; i < m; ++i) {*/
-    /*free(M[i]);*/
-    /*free(copia[i]);*/
-    /*}*/
-    /*free(M);*/
-    /*free(copia);*/
+    for(int i = 0; i < m; ++i) {
+        free(M[i]);
+        free(copia[i]);
+    }
+    free(M);
+    free(copia);
 
     return 0;
 }
