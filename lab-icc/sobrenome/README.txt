d607d887112c7ddc9a24d8db1d832968f94d653f
Eduardofig <duzinho039@gmail.com>
2021-07-20 23:23:34 +0000

    :sparkles: NEW FEATURE: Ataques

---
+++ lab-icc/sobrenome/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 20/07/2021 17:42:43 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [5 - Alocação Dinâmica] Adicionando Sobrenomes
--- lab-icc/sobrenome/main.c
+++ lab-icc/sobrenome/main.c
@@ -4,7 +4,9 @@
 int $ = 0, eol = 0;
 const int mxN = 1e4;
 
-char *read_token(char *delimitadores)
+typedef char* string;
+
+string read_token(string delimitadores)
 {
     size_t tamanho_da_linha = sizeof(char);
     char c, *linha = malloc(tamanho_da_linha), *failsafe;
@@ -12,7 +14,6 @@ char *read_token(char *delimitadores)
         //Quando chega no final da linha retorna a linha e adiciona o '\0' no final
         for(int j = 0; delimitadores[j] != 0; ++j) {
             if(c == delimitadores[j]) {
-                if(c == '\r') scanf("%c", &c);
                 if(c == '$') $ = 1;
                 if(c == '\n') eol = 1;
                 linha[i] = 0;
@@ -33,47 +34,93 @@ char *read_token(char *delimitadores)
     return NULL;
 }
 
-int append_nome(char *nome, char ***lista_de_nomes, int *ultima_posicao)
+//Funcao que da append em um nome completo
+int append_nome(string nome, string **nome_completo, int *ultima_posicao)
 {
     if(nome) {
-        (*lista_de_nomes)[(*ultima_posicao)++] = nome;
-        char **tmp = realloc(*lista_de_nomes, sizeof(char*)*(*ultima_posicao + 1));
+        (*nome_completo)[(*ultima_posicao)++] = nome;
+        string *tmp = (string*)realloc(*nome_completo, sizeof(string)*(*ultima_posicao + 1));
 
         if(!tmp) {
             for(int i = 0; i < *ultima_posicao - 1; ++i) {
-                free(lista_de_nomes[i]);
+                free(nome_completo[i]);
             }
-            free(lista_de_nomes);
+            free(nome_completo);
             exit(0);
         }
 
-        *lista_de_nomes = tmp;
+        *nome_completo = tmp;
+        /*printf("nome: %s\n", (*nome_completo)[*ultima_posicao - 1]);*/
+        /*if(*ultima_posicao > 1) printf("nome - 1: %s\n", (*nome_completo)[*ultima_posicao - 2]); */
+        if($) return 0;
         return 1;
     }
     return 0;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, string argv[])
 {
-    char ***lista_de_nomes = (char***)malloc(sizeof(char**)), *nome;
-    int ultima_posicao[mxN], qtd_nomes = 0;
+    string **lista_de_nomes = (string**)malloc(sizeof(string*)), nome;
+    int n_sobrenomes[mxN], n_nomes_completos, i, j, k;
 
-    for(qtd_nomes = 0; !$; ++qtd_nomes) {
-        lista_de_nomes[qtd_nomes] = (char**)malloc(sizeof(char*));
+    for(n_nomes_completos = 0; !$ && n_nomes_completos < 4; ++n_nomes_completos) {
+        lista_de_nomes[n_nomes_completos] = (string*)malloc(sizeof(string));
         do {
             if(eol) {
                 eol ^= 1;
                 break;
             }
-            nome = read_token(" $");
-        } while(append_nome(nome, lista_de_nomes + qtd_nomes, ultima_posicao + qtd_nomes));
+            nome = read_token(" $\n");
+
+            /*if(n_nomes_completos > 0)*/
+                /*printf("%s  i = %d\n", lista_de_nomes[0][0], n_nomes_completos);*/
+
+        } while(append_nome(nome, lista_de_nomes + n_nomes_completos, n_sobrenomes + n_nomes_completos));
     }
 
-    for(int i = 0; i < qtd_nomes; ++i) {
-        for(int j = 0; j < ultima_posicao[i]; ++j) {
+    for(k = 0; k < n_nomes_completos - 1; ++k) {
+        if(!(k % 2)) {
+            if(n_sobrenomes[k] > 0)
+                append_nome(lista_de_nomes[k][n_sobrenomes[k] - 1], lista_de_nomes + k + 1, n_sobrenomes + k + 1);
+        }
+    }
+
+    for(i = 0; i < n_nomes_completos; ++i) {
+        for(j = 0; j < n_sobrenomes[i]; ++j) {
             printf("%s ", lista_de_nomes[i][j]);
         }
         printf("\n");
     }
+
+    for(; !$; ++n_nomes_completos) {
+        lista_de_nomes[n_nomes_completos] = (string*)malloc(sizeof(string));
+        do {
+            if(eol) {
+                eol ^= 1;
+                break;
+            }
+            nome = read_token(" $\n");
+
+            /*if(n_nomes_completos > 0)*/
+                /*printf("%s  i = %d\n", lista_de_nomes[0][0], n_nomes_completos);*/
+
+        } while(append_nome(nome, lista_de_nomes + n_nomes_completos, n_sobrenomes + n_nomes_completos));
+    }
+
+    for(; k < n_nomes_completos - 1; ++k) {
+        if(!(k % 2)) {
+            if(n_sobrenomes[k] > 0)
+                append_nome(lista_de_nomes[k][n_sobrenomes[k] - 1], lista_de_nomes + k + 1, n_sobrenomes + k + 1);
+        }
+    }
+
+    for(; i < n_nomes_completos; ++i) {
+        for(j = 0; j < n_sobrenomes[i]; ++j) {
+            printf("%s ", lista_de_nomes[i][j]);
+        }
+        printf("\n");
+    }
+    free(lista_de_nomes);
+
     return 0;
 }
