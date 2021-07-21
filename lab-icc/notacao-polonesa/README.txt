640b4047fe52aca8ee413b074eccb6a8d152e20a
Eduardofig <duzinho039@gmail.com>
2021-07-21 00:36:57 +0000

    :rocket: DEPLOY: Notacao polonesa completa

---
+++ lab-icc/notacao-polonesa/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 20/07/2021 21:25:35 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [5 - Alocação Dinâmica] Notação Polonesa Reversa
--- lab-icc/notacao-polonesa/main.c
+++ lab-icc/notacao-polonesa/main.c
@@ -3,55 +3,120 @@
 
 typedef struct _node
 {
-    int num;
+    double num;
     struct _node *next;
 }node;
 
 typedef struct
 {
     node *top;
+    int tamanho;
 }stack;
 
-stack *create_stack()
+//Funcoes da pilha
+stack *criar_stack()
 {
-    return (stack*)malloc(sizeof(stack));
+    stack *st = (stack*)malloc(sizeof(stack));
+    st->tamanho = 0;
+    return st;
 }
 
-void push(int num, stack *st)
+double esta_vazia(stack *st)
+{
+    return st->top == NULL;
+}
+
+void push(double num, stack *st)
 {
     node *top = (node*)malloc(sizeof(node));
     top->num = num;
     top->next = st->top;
     st->top = top;
+    st->tamanho++;
 }
 
 void pop(stack *st)
 {
+    if(esta_vazia(st)) return;
     node *tmp = st->top;
     st->top = tmp->next;
     free(tmp);
+    st->tamanho--;
 }
 
-int peek(stack *st)
+double peek(stack *st)
 {
     return st->top->num;
 }
 
-int is_empty(stack *st)
+
+int tamanho(stack *st)
 {
-    return st->top == NULL;
+    return st->tamanho;
+}
+
+void deletar_stack(stack **st)
+{
+    if(!*st) return;
+    node *tmp = (*st)->top, *tmp_2;
+    while(tmp) {
+        tmp_2 = tmp->next;
+        free(tmp);
+        tmp = tmp_2;
+    }
+    free(*st);
+    *st = NULL;
 }
 
 int main(int argc, char *argv[])
 {
-    stack *st = create_stack();
+    stack *st = criar_stack();
+    char s[100];
+    double a, b;
 
-    for(int i = 0; i < 10; ++i) 
-        push(i, st);
-
-    for(int i = 0; i < 10; ++i) {
-        printf("%d\n", peek(st));
-        pop(st);
+    //Operacoes da notacao polonesa
+    while(scanf("%s", s) == 1) {
+        switch(s[0]) {
+            case '+':
+                b = peek(st);
+                pop(st);
+                a = peek(st);
+                pop(st);
+                push(a + b, st);
+                printf("%lf + %lf =  %lf\n", a, b, peek(st));
+                break;
+            case '-':
+                b = peek(st);
+                pop(st);
+                a = peek(st);
+                pop(st);
+                push(a - b, st);
+                printf("%lf - %lf =  %lf\n", a, b, peek(st));
+                break;
+            case '*':
+                b = peek(st);
+                pop(st);
+                a = peek(st);
+                pop(st);
+                push(a*b, st);
+                printf("%lf * %lf =  %lf\n", a, b, peek(st));
+                break;
+            case '/':
+                b = peek(st);
+                pop(st);
+                a = peek(st);
+                pop(st);
+                push(a/b, st);
+                printf("%lf / %lf =  %lf\n", a, b, peek(st));
+                break;
+            default:
+                push(atof(s), st);
+                break;
+        }
     }
+
+    printf("Resultado: %lf\n", peek(st));
+    deletar_stack(&st);
+
     return 0;
 }
