3062265b1514075d93363d71793c1d7fb412a23a
Eduardofig <duzinho039@gmail.com>
2021-07-23 19:53:36 +0000

    :sparkles: NEW FEATURE: Ataques lab-icc funcionando

--- lab-icc/ataques/main.c
+++ lab-icc/ataques/main.c
@@ -14,28 +14,28 @@ int max(int a, int b)
 
 int main(int argc, char *argv[])
 {
-    int m, **matriz;
+    int m;
+    float **matriz;
     scanf("%d", &m);
 
-    matriz = (int**)malloc(sizeof(int*)*m);
+    matriz = (float**)malloc(sizeof(float*)*m);
     for (int i = 0; i < m; ++i) {
-        matriz[i] = (int*)malloc(sizeof(int)*m);
+        matriz[i] = (float*)malloc(sizeof(float)*m);
         for (int j = 0; j < m; ++j) {
-            scanf("%d", matriz[i] + j);
+            scanf("%f", matriz[i] + j);
         }
     }
-    int a, b, ans = 0, n_ataques = 0, ans = 0;
+    int a, b, n_ataques = 0;
+    float ans = 0;
     ataque *ataques;
     while(1) {
 
-
         scanf("%d", &a);
         if(a == -1) break;
+        scanf("%d", &b);
 
         if(!ataques) ataques = (ataque*)malloc(sizeof(ataque));
         else ataques = (ataque*)realloc(ataques, sizeof(ataque)*(n_ataques + 1));
-
-        scanf("%d", &b);
 
         ataques[n_ataques].poder = a;
         ataques[n_ataques].tipo = b;
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
         ans = max(ans, matriz[ataques[i].tipo][tipo_inimigo]*ataques[i].poder);
     }
 
-    printf("%d\n", ans);
+    printf("%f\n", ans);
 
     return 0;
 }
