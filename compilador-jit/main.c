f59680aeb1d09d467a228b5f02299d412408d2e4
Eduardo Figuiredo Freire Andrade <duzinho039@gmail.com>
2021-07-28 17:50:14 +0000

    compilador terminado

--- compilador-jit/main.c
+++ compilador-jit/main.c
@@ -32,19 +32,25 @@ void transpilar_codigo(FILE *arquivo, char codigo)
 
 int main(int argc, char *argv[])
 {
-    FILE *arquivo = fopen("arquivo.c", "w");
+    FILE *arquivo = fopen("jit.c", "w");
     char c,
          comeco[1000] = "#include <stdio.h>\n\nint main() {\nchar mem[3000];\nint i = 0;\nfor(int j = 0; j < 3000; ++j) {\nmem[j] = 0;\n}\n",
          final[1000] = "printf(\"\\n\");\nfor(int j = 0; j < 3000; ++j){\nif (mem[j] != 0) {\nprintf(\"%d \", mem[j]);\n}\n}\n\nprintf(\"\\n\");\n\nreturn 0;\n}";
 
+    //Escreve o comeco do codigo
     fwrite(comeco, sizeof(char), strlen(comeco), arquivo);
 
+    //Escreve o codigo transpilado
     while(scanf("%c", &c) == 1) 
         transpilar_codigo(arquivo, c);
 
+    //Escreve o final do codigo
     fwrite(final, sizeof(char), strlen(final), arquivo);
 
     fclose(arquivo);
+
+    system("gcc jit.c -o jit.exe");
+    system("./jit.exe");
 
     return 0;
 }
